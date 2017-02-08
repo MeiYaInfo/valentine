40af883ca463fb0ba882d9aad502793218a1cea4
lizhenghua <lizhenghua@360.cn>
2017-02-08 19:18:43 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(function () {
-    console.log(md5('heheh'));
+    var options = '';
+    for (var i = 1; i <= 100; i ++) {
+        options += '<option value="' + i + '">' + i + '</option>';
+    }
+    $('#age').append(options);
+    $('#age').val('20');
+    $('#age').mobiscroll().select({
+        theme: 'mobiscroll',
+        lang: 'zh',
+        display: 'bottom',
+        minWidth: 200
+    });
 })
