7174f9ae06ea074bb5cd1edff74f3527bfbc24ff
lizhenghua <lizhenghua@360.cn>
2017-02-12 17:52:27 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 $(function () {
+
+    // 首次点击年龄载入年龄数据，并设置初始值为20
+    $('#ageinput').click(function () {
+        $('#ageinput').hide();
+        $('#ageselect_dummy').show();
+        $('#ageselect_dummy').click();
+    });
     var options = '';
     for (var i = 1; i <= 100; i ++) {
         options += '<option value="' + i + '">' + i + '</option>';
     }
-    $('#age').append(options);
-    $('#age').val('20');
-    $('#age').mobiscroll().select({
+    $('#ageselect').html(options);
+    $('#ageselect').val('20');
+    $('#ageselect').mobiscroll().select({
         theme: 'mobiscroll',
         lang: 'zh',
         display: 'bottom',
         minWidth: 200
     });
-})
+
+    // 设置性别
+    var sex = 'female';
+    var female = $('.female');
+    var male = $('.male');
+    $('.female').click(function () {
+        if (sex == 'male') {
+            sex = 'female';
+            male.removeClass('selected');
+            female.addClass('selected');
+        }
+    })
+    $('.male').click(function () {
+        if (sex == 'female') {
+            sex = 'male';
+            female.removeClass('selected');
+            male.addClass('selected');
+        }
+    })
+
+    // 点击提交按钮
+    $('.btn-submit').click(function () {
+        var name = $('#name').val(),
+            age = $('#ageselect').val(),
+            sex = $('.sex').find('.selected').data('sex');
+        if (name == '') {
+            return alert('请输入姓名');
+        }
+        if ($('#ageinput').css('display') != 'none') {
+            return alert('请选择年龄');
+        }
+        window.location.href = './result.html?name=' + name + '&age=' + age + '&sex=' + sex;
+    })
+    
+});
+
+
+
+
+
+
+
---
+++ result.js
@@ -0,0 +1,86 @@
+$(function () {
+    var app = new Vue({
+
+        el: 'body',
+
+        data: function () {
+            return {
+                name: 'name',
+                time: 'time',
+                location: 'location',
+                event: 'event',
+                people: 'people'
+            }
+        },
+
+        created: function () {
+            var self = this;
+            this.name = this.GetQueryString('name');
+            this.age = this.GetQueryString('age');
+            this.sex = this.GetQueryString('sex');
+            var queryData = {
+                name: this.name,
+                age: this.age,
+                sex: this.sex
+            };
+            var data = {
+                status: 200,
+                data: {
+                    "time": "time1",
+                    "location": "location2",
+                    "event": "event3",
+                    "people": "people4"
+                },
+                message: 'success'
+            };
+            // $.get('./search', queryData, function (data) {
+                if (data.status == 200) {
+                    self.time = data.data.time;
+                    self.location = data.data.location;
+                    self.event = data.data.event;
+                    self.people = data.data.people;
+                } else {
+                    console.log(data.message);
+                }
+            // });
+        },
+
+        methods: {
+
+            GetQueryString: function (name) {
+                var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+                var r = window.location.search.substr(1).match(reg);
+                if (r != null) {
+                    return unescape(r[2]);
+                };
+                return null;
+            }
+
+        },
+
+        ready: function () {
+            $(".result span").css({
+                fontSize: $(".result span").height() * 0.7 + "px",
+                lineHeight: $(".result span").height() + "px"
+            });
+            var img = new Image();
+            img.src = "./save.png";
+            $('.save').click(function () {
+                $('.mcover').fadeIn();
+                // $('.mcover').css('display', 'block');
+            })
+
+            // 点击关闭模态框
+            $('.mclose').click(function () {
+                $('.mcover').hide();
+            })
+
+            // 再测一次
+            $('.repeat').click(function () {
+                window.location.href = './index.html';
+            })
+
+        }
+
+    })
+})
